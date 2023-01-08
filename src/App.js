4751acfa0f0a205eb6a73ea740bfd0a87bd056ec
Winston <wlnson1@gmail.com>
2023-01-08 02:50:21 -0800

    added handleclick and images array

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 // IMPORT useState
 import React from 'react';
+import { useState } from 'react';
 import './styles.css';
 // IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+import imagesArr from "./imageData.js"
+
 
 export default function App() {
   // USE useState TO CREATE  [bigImage, setBigImage] 
   // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+  [bigImage, setBigImage] = useState(imagesArr[0].img)
 
   // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  const handleImgClick = (url) => {
+    setBigImage(url);
+  }
 
   // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
   // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+  const images = imagesArr.map((image, idx) => {
+    return ( 
+      <img 
+        src={image.img}
+        alt={image.city}
+        key={idx}
+        className='image'
+        onClick={() => handleImgClick(image.img)}
+
+        ></img>
+    )
+  })
+
 
   return (
     <div className="App">
